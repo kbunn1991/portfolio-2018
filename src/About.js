4870f9b5b1bd4f20e26df5a4427e6c04d6c577aa
Kaitlin Bunn <kabunn1991@gmail.com>
2018-10-19 14:37:46 -0400

    fleshing out projects

--- src/About.js
+++ src/About.js
@@ -14,27 +14,61 @@ class About extends React.Component {
                 <div className="pTitle"><span>Party Up!</span></div>
                 <div className="pInfo">A teamfinding app for PS4.<br /><br />
                 <span>frontend</span>: react<br />
-                <span>backend</span>: blah blah</div>
+                <span>backend</span>: blah blah
+
+                <div className="buttonz"><a href="https://partyup-client.herokuapp.com/"><span>live</span></a> <a href="https://github.com/kbunn1991/kaitlin-partyup-client"><span>github</span></a></div>
+
+                </div>
                 </div>
               <div className="portBox pic2">
                 <div className="pTitle"><span>Overwatch App</span></div>
                 <div className="pInfo">Information on Overwatch heroes.<br /> <br />
                 <span>frontend</span>: react<br />
-                backend: n/a</div>
+                backend: n/a
+                
+                <div className="buttonz"><span>live</span> <span>github</span></div>
+
+              </div>
               </div>
               <div className="portBox pic3">
-                <div className="pTitle"><span>Noteful</span></div>
+                <div className="pTitle"><span>Bunny Babbel</span></div>
                 <div className="pInfo">
-                  A notes app to create and store.<br /> <br />
+                  A language app coded in a team.<br /> <br />
                   <span>frontend</span>: html5, css<br />
                   backend: n/a
+
+                  <div className="buttonz"><a href="https://bunneh-client.herokuapp.com/"><span>live</span></a> <a href="https://github.com/kbunn1991/Lapine-DonnyKaitlin-Client"><span>github</span></a></div>
+
                 </div>
               </div>
             </div>
             <div className="portBoxRow">
-              <div className="portBox pic4"></div>
-              <div className="portBox pic5"></div>
-              <div className="portBox pic6"></div>
+              <div className="portBox pic4">
+                <div className="pTitle"><span>Weather FM</span></div>
+                  <div className="pInfo">Mood music app coded in a team.<br /> <br />
+                  <span>frontend</span>: react<br />
+                  backend: n/a
+                  
+                  <div className="buttonz"><span>live</span> <span>github</span></div>
+
+                  </div>
+              </div>
+              <div className="portBox pic5">
+              <div className="pTitle"><span>Portfolio 2018</span></div>
+                <div className="pInfo">Portfolio coded in React.<br /> <br />
+                <span>frontend</span>: react<br />
+                backend: n/a
+                
+                <div className="buttonz"><span>live</span> <span>github</span></div>
+
+                </div>
+              </div>
+              <div className="portBox pic6">
+                <div className="pTitle"><span>TBA</span></div>
+                <div className="pInfo">Coming soon!<br /> <br />
+                <br />
+                eyes emoji</div>
+                </div>
             </div>
           </div>
 
--- src/Header.js
+++ src/Header.js
@@ -14,11 +14,12 @@ class Header extends React.Component {
           kait<span>lin</span> &lt;/&gt;
         </h1>
         <div className="underInfo">
-          Hello world! I'm Kait — an ATL-based full-stack developer and designer that makes beautiful, responsive websites and web applications. <span>Let's chat!</span>
+          Hello world! I'm Kait — an ATL-based full-stack developer and designer that makes beautiful, responsive websites and web applications. <a href="#contact"><span>Let's chat!</span></a>
 
-          <p className="skillz type-it">let <span>languages</span> = [css, HTML5, javascript, node.js];<br />
-          let <span>databases</span> = [sql, mongodb];<br />
-          let <span>libraries</span> = [react];</p>
+          <p className="skillz type-it">const <span>languages</span> = [css, HTML5, javascript, node.js];<br />
+          const <span>databases</span> = [sql, mongodb];<br />
+          const <span>libraries</span> = [react, redux];<br />
+          const <span>tools</span> = [developer tools, github, postman]</p>
         </div>
       </div>
     )
